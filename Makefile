28c2288309a5087a7c3f8161ff97a2d9f4dd17d7
ybod <yuriibodarev@gmail.com>
2017-11-27 19:24:56 +0200

    Erland and Elixir version updated, build and tests fixed

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ ERL_INCLUDE_PATH=$(shell erl -eval 'io:format("~s~n", [lists:concat([code:root_d
 all: priv/digital_signature_lib_nif.so
 
 priv/digital_signature_lib_nif.so: src/digital_signature_lib_nif.c
-	cc -Wall -Wextra -Wno-unused-parameter -Wl,-undefined -Wl,dynamic_lookup -shared -fPIC -I$(ERL_INCLUDE_PATH) src/digital_signature_lib_nif.c -o priv/digital_signature_lib_nif.so
+	cc -Wall -Wno-unused-parameter -Wl,-undefined -Wl,dynamic_lookup -shared -fPIC -I$(ERL_INCLUDE_PATH) src/digital_signature_lib_nif.c -o priv/digital_signature_lib_nif.so
--- test/digital_signature_test.exs
+++ test/digital_signature_test.exs
@@ -3,18 +3,10 @@ defmodule DigitalSignatureLibTest do
   doctest DigitalSignatureLib
 
   test "fail" do
-    assert DigitalSignatureLib.processPKCS7Data([], %{general: [], tsp: []}, 1) == {:error, "pkcs7 data is empty"}
+    assert DigitalSignatureLib.processPKCS7Data([], %{general: [], tsp: []}, 1) == {:error, 'pkcs7 data is empty'}
   end
 
   test "ok" do
-    assert DigitalSignatureLib.processPKCS7Data([<<1>>], %{general: [], tsp: []}, 1) == {:ok, %{}}
+    assert {:ok, %{}} = DigitalSignatureLib.processPKCS7Data([<<1>>], %{general: [], tsp: []}, 1)
   end
 end
-
-# -module(digital_signature_lib_tests).
-
-# -include_lib("eunit/include/eunit.hrl").
-
-# some_test() ->
-#     ?assertMatch({error, "pkcs7 data is empty"}, digital_signature_lib:processPKCS7Data([], #{general => [], tsp => []}, 1)),
-#     ?assertMatch({ok, #{}}, digital_signature_lib:processPKCS7Data([<<1>>], #{general => [], tsp => []}, 1)).
